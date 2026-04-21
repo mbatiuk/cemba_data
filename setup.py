392f4596cc0a21d9851563ab8553da1d7e2e0557
Mykhailo <mykhailo.batiuk@protonmail.ch>
2026-04-21 16:15:06 +0200

    Legacy cleanup

--- setup.py
+++ setup.py
@@ -2,37 +2,31 @@ from setuptools import setup, find_packages
 
 setup(
     name='cemba_data',
-	use_scm_version=True, #{'version_scheme': 'python-simplified-semver',"local_scheme": "no-local-version"},
-	# version = "1.8.1",
-    setup_requires=['setuptools_scm'],
+    version='2.0.0',
     author='Hanqing Liu, Wubin Ding',
     author_email='hanliu@salk.edu, wding@salk.edu',
-    description='Pipelines for single nucleus methylome and multi-omic dataset.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/DingWB/cemba_data; https://github.com/lhqing/cemba_data',
+    maintainer='Mykhailo Batiuk',
+    maintainer_email='mykhailo.batiuk@protonmail.ch',
+    description='Pipelines for single nucleus methylome and multi-omic dataset',
+    url='https://github.com/mbatiuk/cemba_data',
     license='MIT',
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.9",
     ],
     packages=find_packages(exclude=('doc',)),
     include_package_data=True,
     package_data={
-        '': ['*.txt', '*.tsv', '*.csv', '*.fa', '*Snakefile', '*ipynb','*.smk']
+        '': ['*.txt', '*.tsv', '*.csv', '*.fa', '*Snakefile', '*ipynb', '*.smk']
     },
-    install_requires=['pandas>=1.0',
-                      'numpy',
-                      'seaborn',
-                      'matplotlib',
-                      'papermill',
-                      'dnaio',
-                      'pysam'],
+    install_requires=[],
     entry_points={
-        'console_scripts': ['yap=cemba_data.__main__:main',
-                            'yap-internal=cemba_data._yap_internal_cli_:internal_main',
-                            'yap-hisat3n=cemba_data.hisat3n.cli:main',
-							'yap-gcp=cemba_data.gcp:main'],
+        'console_scripts': [
+            'yap=cemba_data.__main__:main',
+            'yap-internal=cemba_data._yap_internal_cli_:internal_main',
+            'yap-hisat3n=cemba_data.hisat3n.cli:main',
+            'yap-gcp=cemba_data.gcp:main',
+        ]
     }
 )
