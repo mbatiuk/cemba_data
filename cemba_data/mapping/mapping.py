673f7cfbfd0cf511b9fc910297bd835c8a1a71e3
Mykhailo <mykhailo.batiuk@protonmail.ch>
2026-05-07 16:29:30 +0200

    Remove unused config parameters never passed to mapping smk

--- cemba_data/mapping/mapping.py
+++ cemba_data/mapping/mapping.py
@@ -183,69 +183,6 @@ def mapping(output_dir,
 			os.system(cmd)
 
 
-def m3c_config_str(config):
-    """Change the dtype of parameters and make a appropriate string"""
-    int_parameters = {
-        'overlap': 6,
-        'r1_left_cut': 10,
-        'r1_right_cut': 10,
-        'r2_left_cut': 10,
-        'r2_right_cut': 10,
-        'quality_threshold': 20,
-        'length_threshold': 30,
-        'total_read_pairs_min': 1,
-        'total_read_pairs_max': 6000000,
-        'mapq_threshold': 10,
-        'num_upstr_bases': 0,
-        'num_downstr_bases': 2,
-        'compress_level': 5,
-        'split_left_size': 40,
-        'split_right_size': 40,
-        'split_middle_min_size': 30,
-        'min_gap': 2500,
-        'trim_on_both_end': 5
-    }
-
-    str_parameters = {
-        'mode': 'm3c',
-        'r1_adapter': 'AGATCGGAAGAGCACACGTCTGAAC',
-        'r2_adapter': 'AGATCGGAAGAGCGTCGTGTAGGGA',
-        'hisat3n_dna_reference': 'required',
-        'hisat3n_repeat_index_type': 'no-repeat',
-        'reference_fasta': 'required',
-        'mc_stat_feature': 'CHN CGN CCC',
-        'mc_stat_alias': 'mCH mCG mCCC',
-        'chrom_size_path': 'required',
-        'annotation_path': None
-    }
-
-    typed_config = {}
-    for k, default in int_parameters.items():
-        if k in config:
-            typed_config[k] = int(config[k])
-        else:
-            if default != 'required':
-                typed_config[k] = default
-            else:
-                raise ValueError(f'Required parameter {k} not found in config. '
-                                 f'You can print the newest mapping config template via "yap default-mapping-config".')
-
-    for k, default in str_parameters.items():
-        if k in config:
-            typed_config[k] = f"'{config[k]}'"
-        else:
-            if default != 'required':
-                typed_config[k] = f"'{default}'"
-            else:
-                raise ValueError(f'Required parameter {k} not found in config. '
-                                 f'You can print the newest mapping config template via "yap default-mapping-config".')
-
-    config_str = ""
-    for k, v in typed_config.items():
-        config_str += f"{k} = {v}\n"
-    return config_str
-
-
 def bam_read_to_fastq_read(read, read_type=None):
 	if read_type is None:
 		if read.is_read1:
@@ -347,6 +284,70 @@ def separate_unique_and_multi_align_reads(in_bam_path,
 	return
 
 
+def m3c_config_str(config):
+    """Change the dtype of parameters and make a appropriate string"""
+    int_parameters = {
+        'overlap': 6,
+        'r1_left_cut': 10,
+        'r1_right_cut': 10,
+        'r2_left_cut': 10,
+        'r2_right_cut': 10,
+        'quality_threshold': 20,
+        'length_threshold': 30,
+        'mapq_threshold': 10,
+        'num_upstr_bases': 0,
+        'num_downstr_bases': 2,
+        'compress_level': 5,
+        'split_left_size': 40,
+        'split_right_size': 40,
+        'split_middle_min_size': 30,
+        'min_gap': 2500,
+        'trim_on_both_end': 5
+    }
+
+    str_parameters = {
+        'mode': 'm3c',
+        'r1_adapter': 'AGATCGGAAGAGCACACGTCTGAAC',
+        'r2_adapter': 'AGATCGGAAGAGCGTCGTGTAGGGA',
+        'hisat3n_dna_reference': 'required',
+        'hisat3n_repeat_index_type': 'no-repeat',
+        'reference_fasta': 'required',
+        'mc_stat_feature': 'CHN CGN CCC',
+        'mc_stat_alias': 'mCH mCG mCCC',
+        'chrom_size_path': 'required',
+        'annotation_path': None
+    }
+
+    typed_config = {}
+    for k, default in int_parameters.items():
+        if k in config:
+            typed_config[k] = int(config[k])
+        else:
+            if default != 'required':
+                typed_config[k] = default
+            else:
+                raise ValueError(f'Required parameter {k} not found in config. '
+                                 f'You can print the newest mapping config template via "yap default-mapping-config".')
+
+    for k, default in str_parameters.items():
+        if k in config:
+            typed_config[k] = f"'{config[k]}'"
+        else:
+            if default != 'required':
+                typed_config[k] = f"'{default}'"
+            else:
+                raise ValueError(f'Required parameter {k} not found in config. '
+                                 f'You can print the newest mapping config template via "yap default-mapping-config".')
+
+    config_str = ""
+    for k, v in typed_config.items():
+        config_str += f"{k} = {v}\n"
+    return config_str
+
+
+
+
+
 def mc_config_str(config):
     """Change the dtype of parameters and make a appropriate string"""
     int_parameters = {
@@ -357,15 +358,11 @@ def mc_config_str(config):
         'r2_right_cut': 10,
         'quality_threshold': 20,
         'length_threshold': 30,
-        'total_read_pairs_min': 1,
-        'total_read_pairs_max': 6000000,
         'mapq_threshold': 10,
         'num_upstr_bases': 0,
         'num_downstr_bases': 2,
         'compress_level': 5
     }
-
-    bool_parameters = {'unmapped_fastq': False}
 
     str_parameters = {
         'mode': 'mc',
@@ -389,24 +386,6 @@ def mc_config_str(config):
                 typed_config[k] = default
             else:
                 raise ValueError(f'Required parameter {k} not found in config.')
-
-    for k, default in bool_parameters.items():
-        if k in config:
-            v = config[k]
-            if v.lower().startswith('t'):
-                v = True
-            else:
-                v = False
-            typed_config[k] = v
-        else:
-            if default != 'required':
-                typed_config[k] = default
-            else:
-                raise ValueError(f'Required parameter {k} not found in config. '
-                                 f'You can print the newest mapping config template via "yap default-mapping-config".')
-    # judge unmapped_fastq specifically
-    unmapped_param_str = '--un' if typed_config['unmapped_fastq'] else ''
-    typed_config['unmapped_param_str'] = f"'{unmapped_param_str}'"
 
     for k, default in str_parameters.items():
         if k in config:
@@ -434,8 +413,6 @@ def mct_config_str(config):
         'r2_right_cut': 10,
         'quality_threshold': 20,
         'length_threshold': 30,
-        'total_read_pairs_min': 1,
-        'total_read_pairs_max': 6000000,
         'mapq_threshold': 10,
         'num_upstr_bases': 0,
         'num_downstr_bases': 2,
@@ -448,8 +425,6 @@ def mct_config_str(config):
         'mc_rate_max_threshold': 0.5,
         'mc_rate_min_threshold': 0.9
     }
-    bool_parameters = {'unmapped_fastq': False}
-
     str_parameters = {
         'mode': 'mct',
         'r1_adapter': 'AGATCGGAAGAGCACACGTCTGAAC',
@@ -485,24 +460,6 @@ def mct_config_str(config):
                 typed_config[k] = default
             else:
                 raise ValueError(f'Required parameter {k} not found in config.')
-
-    for k, default in bool_parameters.items():
-        if k in config:
-            v = config[k]
-            if v.lower().startswith('t'):
-                v = True
-            else:
-                v = False
-            typed_config[k] = v
-        else:
-            if default != 'required':
-                typed_config[k] = default
-            else:
-                raise ValueError(f'Required parameter {k} not found in config. '
-                                 f'You can print the newest mapping config template via "yap default-mapping-config".')
-    # judge unmapped_fastq specifically
-    unmapped_param_str = '--un' if typed_config['unmapped_fastq'] else ''
-    typed_config['unmapped_param_str'] = f"'{unmapped_param_str}'"
 
     for k, default in str_parameters.items():
         if k in config:
